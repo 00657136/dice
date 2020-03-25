33c0111fc221f631ecda85215737bd29c8f58028
NTOU <fus22803281@gmail.com>
2020-03-25 14:38:38 +0800

    3.25

--- dice/ContentView.swift
+++ dice/ContentView.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    @State private var Num : Int = 0
+    @State private var color:[Color] = [.black,.blue,.red,.green,.orange,.yellow,.purple]
     var body: some View {
-        Text("Hello World")
+        ZStack{
+            Image("star").resizable().scaledToFill().frame(minWidth: 0, maxWidth: .infinity)
+        VStack(alignment: .leading){
+            ZStack{
+                Circle().frame(width:200,height:200).foregroundColor(color[Num])
+                Text("\(Num)").font(.system(size: 160)).foregroundColor(Color.white)
+            }
+            Button(action:{
+                let randNum = 1...6
+                self.Num = randNum.randomElement()!
+                withAnimation(.easeOut(duration: 0.3)){
+                    self.animationAmount -= 360
+                }
+            }){
+                Image("casino").renderingMode(.original).resizable().frame(width: 100,height: 100).scaledToFit().shadow(color: color[Num], radius: 50).rotation3DEffect(.degrees(animationAmount), axis: (x: 0.5, y: 0.5, z: 0.5)).opacity(1)
+                Text("roll").font(.system(size: 70)).foregroundColor(color[Num])
+            }
+        }
+        
+        
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
