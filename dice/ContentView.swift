b9b41de8212e098acba1f154eb92613d51ed0ca3
NTOU <fus22803281@gmail.com>
2020-03-25 12:28:38 +0800

    Initial Commit

---
+++ dice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dice
+//
+//  Created by User17 on 2020/3/25.
+//  Copyright © 2020 NTOU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
