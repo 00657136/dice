91f5a4ddc15141d5f78ce7e7cf0abe7d9af30747
Claudio <jeff.797886@gmail.com>
2020-03-25 15:28:54 +0800

    yeah

--- dice/ContentView.swift
+++ dice/ContentView.swift
@@ -9,13 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
     @State private var animationAmount = 0.0
     @State private var Num : Int = 0
     @State private var color:[Color] = [.black,.blue,.red,.green,.orange,.yellow,.purple]
+    @State private var lunch = ["無","炒飯","肉燥飯","雞肉飯","雞腿飯","雞排飯","燴飯"]
     var body: some View {
-        ZStack{
+        ZStack(alignment: .bottom){
             Image("star").resizable().scaledToFill().frame(minWidth: 0, maxWidth: .infinity)
-        VStack(alignment: .leading){
+        VStack(alignment: .center){
+            Text("讓蛇丸幫你決定今天吃什麼？").font(.system(size: 50)).foregroundColor(Color.white)
+                .padding(.bottom)
+            
+            
             ZStack{
                 Circle().frame(width:200,height:200).foregroundColor(color[Num])
                 Text("\(Num)").font(.system(size: 160)).foregroundColor(Color.white)
@@ -23,13 +29,22 @@ struct ContentView: View {
             Button(action:{
                 let randNum = 1...6
                 self.Num = randNum.randomElement()!
-                withAnimation(.easeOut(duration: 0.3)){
+                withAnimation(.default){
                     self.animationAmount -= 360
                 }
+                self.showAlert = true
             }){
-                Image("casino").renderingMode(.original).resizable().frame(width: 100,height: 100).scaledToFit().shadow(color: color[Num], radius: 50).rotation3DEffect(.degrees(animationAmount), axis: (x: 0.5, y: 0.5, z: 0.5)).opacity(1)
-                Text("roll").font(.system(size: 70)).foregroundColor(color[Num])
+                Image("casino").renderingMode(.original).resizable().frame(width: 100,height: 100).scaledToFit().shadow(color: color[Num], radius: 50).rotation3DEffect(.degrees(animationAmount), axis: (x: 0.5, y: 0.5, z: 0.5))
+                Text("我骰!").font(.system(size: 50)).foregroundColor(.black).padding().background(LinearGradient(gradient: Gradient(colors: [Color.init(red: 255/255, green: 217/255, blue: 218/255), Color.init(red: 204/255, green: 113/255, blue: 120/255)]), startPoint: UnitPoint(x: 0, y: 0), endPoint: UnitPoint(x: 1, y: 1))).cornerRadius(20)
+                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 5))
+                
+                
+            }.alert(isPresented: $showAlert) { () -> Alert in
+               
+               return Alert(title: Text(lunch[Num]))
             }
+            
+            Image("蛇丸").resizable().scaledToFill().frame(width: 180,height: 180)
         }
         
         
